2d13840188f779f2a79f79e6495287a80d4eecce
sun200930 <45586508+sun200930@users.noreply.github.com>
2021-06-29 10:56:03 +0800

    Update SV.main.sh

--- structural_variation/SV.main.sh
+++ structural_variation/SV.main.sh
@@ -32,8 +32,10 @@ for line in $(seq 1 $sample_num); do
   # run Manta 
   $Bin/configManta.py --bam $bam --referenceFasta $genome --runDir=$outdir/Manta_BreakDancer/$id
   python2 $outdir/Manta_BreakDancer/$id/runWorkflow.py
+  
   ## convert INV, and only retain INS, DEL and INV 
   python2 $Bin/convertInversion.py $Bin/samtools $genome $outdir/Manta_BreakDancer/$id/results/variants/candidateSV.vcf.gz > $outdir/Manta_BreakDancer/$id/candidateSV.vconvertINV.vcf
+  
   # split by SV type: INS, DEL, and INV
   grep -E 'SVTYPE=INS' $outdir/Manta_BreakDancer/$id/candidateSV.vconvertINV.vcf > $outdir/Manta_BreakDancer/$id/INS.vcf
   grep -E 'SVTYPE=DEL' $outdir/Manta_BreakDancer/$id/candidateSV.vconvertINV.vcf > $outdir/Manta_BreakDancer/$id/DEL.vcf
@@ -43,14 +45,25 @@ for line in $(seq 1 $sample_num); do
   ## Generating configuration file
   perl $Bin/bam2cfg.pl -v 100 -t $Bin/samtools -q 20 -o $outdir/Manta_BreakDancer/$id/breakdancer.cfg -h $bam
   $Bin/breakdancer-max -m 10000000 -q 20 -y 20 -r 1 $outdir/Manta_BreakDancer/$id/breakdancer.cfg 1> $outdir/Manta_BreakDancer/$id/breakdancer.ctx 2> $outdir/Manta_BreakDancer/$id/breakdancer.log
-  ## only retain ITX and CTX, at least three reads to support this ITX and CTX
+ 
+  ## only retain ITX, at least three reads to support this ITX and CTX
   grep -E "^#|ITX|CTX" $outdir/Manta_BreakDancer/$id/breakdancer.ctx | perl -ne '$num=$1 if (/bam\|(\d+)/); print if ($num>=3)' > $outdir/Manta_BreakDancer/$id/breakdancer.filt.ctx
   perl $Bin/ctx_2_vcf.pl $outdir/Manta_BreakDancer/$id/breakdancer.filt.ctx $outdir/Manta_BreakDancer/$id/breakdancer.filt.vcf
   grep -E 'SVTYPE=ITX' $outdir/Manta_BreakDancer/$id/breakdancer.filt.vcf > $outdir/Manta_BreakDancer/$id/ITX.vcf
-  grep -E 'SVTYPE=CTX' $outdir/Manta_BreakDancer/$id/breakdancer.filt.vcf > $outdir/Manta_BreakDancer/$id/CTX.vcf
+  #grep -E 'SVTYPE=CTX' $outdir/Manta_BreakDancer/$id/breakdancer.filt.vcf > $outdir/Manta_BreakDancer/$id/CTX.vcf # not consider CTX
+done
+
+# Step2. merge SV using SURVIVOR, and filter MAF (>0.01)
+for type in INS DEL INV ITX; do
+  vcf_list=''
+  for line in $(seq 1 $sample_num); do
+    id=`head -n $line $bam_list | tail -n 1 | cut -f 1`
+    vcf_list="$vcf_list $outdir/Manta_BreakDancer/$id/$type.vcf"
+  done
   
-  # merge using SUVOVOR
+  # merge
+  $Bin/SURVIVOR merge $vcf_list 1000 1 1 1 0 50 $outdir/$type.merged.vcf
   
   # filter MAF 0.01
-  
-done
+  $Bin/vcftools --vcf $outdir/$type.merged.vcf --maf 0.01 --max-maf 0.99 --recode --recode-INFO-all --out $outdir/$type.final.vcf
+ done
